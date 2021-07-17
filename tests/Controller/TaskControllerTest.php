c3d7e8dd95c32091485bd6074881b41bc191ee40
riwalenn <7VdDQCwL3L&M6#q>
2021-07-17 18:18:52 +0200

    [ADD] controllers tests 1/2

---
+++ tests/Controller/TaskControllerTest.php
@@ -0,0 +1,45 @@
+<?php
+
+
+namespace App\Tests\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class TaskControllerTest extends WebTestCase
+{
+    public function testList()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/tasks');
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+    }
+
+    public function testIsDoneList()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/tasks/done');
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+    }
+
+    public function testCreateTask()
+    {
+
+    }
+
+    public function testEditTask()
+    {
+
+    }
+
+    public function testToggleTask()
+    {
+
+    }
+
+    public function testDeleteTask()
+    {
+
+    }
+}
