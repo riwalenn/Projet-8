5b43496683753fc6508e0bc2ffd65922feff8dc5
riwalenn <7VdDQCwL3L&M6#q>
2021-07-27 17:01:05 +0200

    [ADD] functionnal TaskControllerTest

--- tests/Controller/TaskControllerTest.php
+++ tests/Controller/TaskControllerTest.php
@@ -3,43 +3,101 @@
 
 namespace App\Tests\Controller;
 
-
+use App\Entity\User;
+use App\Tests\NeedLogin;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Response;
 
 class TaskControllerTest extends WebTestCase
 {
-    public function testList()
+    use NeedLogin;
+
+    protected function getEntity($username)
+    {
+        return self::$container->get('doctrine')->getManager()->getRepository(User::class)->findOneBy(['username' => $username]);
+    }
+
+    /**
+     * @param $uri
+     */
+    protected function testWithoutCredentials($uri)
     {
         $client = static::createClient();
-        $client->request('GET', '/tasks');
-        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+        $client->request('GET', $uri);
+        $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
+        $this->assertResponseRedirects('/login');
+        $client->followRedirect();
+        $this->assertSelectorTextContains('button', 'Se connecter');
     }
 
-    public function testIsDoneList()
+    /**
+     * @param $role
+     * @param $uri
+     * @param int $http_response
+     */
+    protected function testWithCredentials($role, $uri, int $http_response = Response::HTTP_OK)
     {
         $client = static::createClient();
-        $client->request('GET', '/tasks/done');
-        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+        $user = $this->getEntity($role);
+        $this->login($client, $user);
+
+        $client->request('GET', $uri);
+
+        $this->assertResponseStatusCodeSame($http_response);
     }
 
-    /*public function testCreateTask()
+    public function testUriList()
+    {
+        $this->testWithoutCredentials('/tasks');
+        $this->testWithCredentials('user','/tasks');
+        $this->testWithCredentials('admin','/tasks');
+    }
+
+    public function testUriIsDoneList()
+    {
+        $this->testWithoutCredentials('/tasks/done');
+        $this->testWithCredentials('user','/tasks/done');
+        $this->testWithCredentials('admin','/tasks/done');
+    }
+
+    public function testUriCreate()
+    {
+        $this->testWithoutCredentials('/tasks/create');
+        $this->testWithCredentials('user','/tasks/create');
+        $this->testWithCredentials('admin','/tasks/create');
+    }
+
+    public function testUriEdit()
+    {
+        $this->testWithoutCredentials('/tasks/1/edit');
+        $this->testWithCredentials('user','/tasks/1/edit');
+        $this->testWithCredentials('admin','/tasks/1/edit');
+    }
+
+    public function testUriToggle()
+    {
+        $this->testWithoutCredentials('/tasks/1/toggle');
+        $this->testWithCredentials('user','/tasks/1/toggle', Response::HTTP_FOUND);
+        $this->testWithCredentials('admin','/tasks/1/toggle', Response::HTTP_FOUND);
+    }
+
+    public function testDeleteWithoutCredential()
     {
 
     }
 
-    public function testEditTask()
+    public function testDeleteWithCredential()
     {
 
     }
 
-    public function testToggleTask()
+    public function testDeleteAnonymousWithoutCredential()
     {
 
     }
 
-    public function testDeleteTask()
+    public function testDeleteAnonymousWithCredential()
     {
 
-    }*/
+    }
 }
